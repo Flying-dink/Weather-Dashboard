d5ddf0a822f96f4ee124da550159de0060ea54ab
gail enstice <skyedoll@aol.com>
2021-07-23 20:51:49 -0400

    brought over weather API

--- weather.js
+++ weather.js
@@ -1,2 +1,18 @@
-// set up searchbox
-// set up 5 day card or grid
+//set up variables
+var city="";
+var APIkey="";
+var cities=document.getElementById("searched-cities-container");
+var cities=("");
+listClicker();
+searchClicker();
+
+//render buttons for each element 
+function renderButtons() {
+    cities.innerHTML = "";
+    if(cities ==null) {
+        return;
+    }
+}
+url = https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+APIkey = "2cf9a1c2beb1b1fc37f62405ffef77ee"
+queryurl = url + city + APIkey
